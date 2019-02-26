0a6fe2e80c43d802746becb32fcb43335a5a1e63
Fanny Maurel <fannymaurel@outlook.com>
2019-02-26 12:12:46 +0100

    Users in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Review.destroy_all
+Booking.destroy_all
+Trip.destroy_all
+
+user1 = User.create!(
+  first_name: 'Kathy',
+  last_name: 'Doe',
+  address: '107 cours Balguerie Stuttenberg 33300 Bordeaux',
+  category: 'fêtarde'
+  email: 'kathy@kathy.com'
+  password: 'topsecret'
+  )
+
+user2 = User.create!(
+  first_name: 'Claudia',
+  last_name: 'Doedoe',
+  address: '102 cours Balguerie Stuttenberg 33300 Bordeaux',
+  category: 'arsouille'
+  email: 'claudia@claudia.com'
+  password: 'topsecret'
+  )
