b70480fbb5974163223d5852dda8bd4b1ce83ddf
Pierre Liduena <pierre@assurancesvelo.com>
2019-03-01 16:26:02 +0100

    video working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,9 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
-
   def home
   end
 
   def video
-    require "opentok"
     @session_id = params[:session_id]
     @token = params[:token]
   end
