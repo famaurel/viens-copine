1196c924975dd60eb84cf0d85f9e5de597034acc
Pierre Liduena <pierre@assurancesvelo.com>
2019-03-04 11:00:32 +0100

    change logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :first_name, :last_name, :address])
   end
 
   def after_sign_in_path_for(resource)
