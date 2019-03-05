51e0b97b1198664850816dd1fde1f59a90196e1f
Pierre Liduena <pierre@assurancesvelo.com>
2019-03-05 15:16:25 +0100

    resolve email bug

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,7 +15,7 @@ class BookingsController < ApplicationController
 
       @publisher_video_url = video_path + "?session_id=#{@session_id}&token=#{@publisher_token}"
       @subscriber_video_url = video_path + "?session_id=#{@session_id}&token=#{@subscriber_token}"
-      UserMailer.booking(current_user, @trip, @subscriber_video_url).deliver_now
+      #UserMailer.booking(current_user, @trip, @subscriber_video_url).deliver_now
       redirect_to @publisher_video_url
       send_sms_to_creator(@trip, @subscriber_video_url)
       flash[:notice] = "Vous avez bien rejoint le trip 🙂 attente de la copine pour le chat"
