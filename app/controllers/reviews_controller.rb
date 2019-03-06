d73077a2288da5d076f4610f5a324a7d536da905
jean marc graufogel <gfl.jm2@wanadoo.fr>
2019-03-06 10:09:54 +0100

    ok for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,14 +2,16 @@ class ReviewsController < ApplicationController
 
   def new
     @booking = Booking.find(params[:booking_id])
-    @review = Review.new(@booking)
+    @review = Review.new(booking_id: @booking.id)
   end
 
   def create
+    @review = Review.new(review_params)
     @booking = Booking.find(params[:booking_id])
-
-    if @booking.update(review_params)
-      redirect_to bookings_path
+    @review.user = current_user
+    @review.booking = @booking
+    if @review.save
+      redirect_to trips_path
     else
       render :new
     end
@@ -17,7 +19,31 @@ class ReviewsController < ApplicationController
 
 private
   def review_params
-    params.require(:booking).permit(:review, :rating)
+    params.require(:review).permit(:content, :rating)
   end
 
 end
+
+
+
+
+#  def create
+#    @sale = Sale.new(sale_params)
+#    @sale.contact = Contact.find(params[:contact_id])
+#    if @sale.save
+#      redirect_to contact_path(params[:contact_id])
+#    else
+#      render :new
+#    end
+#  end
+
+
+
+#  private
+
+#  def sale_params
+#    params.require(:sale).permit(:value, :amount)
+#  end
+
+
+# end
