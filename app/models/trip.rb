2d75dc68972cc68c78f513bacb36ccc016f6e4a0
Fanny Maurel <fannymaurel@outlook.com>
2019-03-07 10:21:48 +0100

    Afficher le bon transport + show en français

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
-  enum transport: { bike: 0, walk: 1, bus: 2 }
+  enum transport: { walk: 0, bike: 1, bus: 2 }
 
   belongs_to :start_address, :class_name => 'Address'
   belongs_to :end_address, :class_name => 'Address'
