8dab0e9b385568213aa2816050610ff943b7d9e4
jean marc graufogel <gfl.jm2@wanadoo.fr>
2019-03-07 18:40:33 +0100

    ok for cocktail category and registration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,4 @@
 module ApplicationHelper
-  def category_icon(category)
-    icons  = {
-      'fêtarde' => "fas fa-cocktail",
-      'calme' => "fa-glass-whiskey"
-    }
-    return icons[category] || "fas fa-wine-glass-alt"
-  end
 
   def transport_icon(number)
     icons = ["fas fa-walking","fas fa-bicycle", "fas fa-bus-alt"]
